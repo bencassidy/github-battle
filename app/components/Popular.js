d6230183c208cdee2f7cbd9dd382ed8e69cc7698
bencassidy <ben.cassidy11@gmail.com>
2017-09-14 20:49:05 -0700

    this keyword tasks

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,9 +1,39 @@
 const React = require('react');
 
 class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLanguage: 'All'
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(lang) {
+    this.setState(() => {
+      return {
+        selectedLanguage: lang
+      }
+    });
+  }
+
   render() {
+    const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
     return (
-      <div>Popular</div>
+      <ul className='languages'>
+        {languages.map((lang) => {
+          return (
+            <li
+              style={lang === this.state.selectedLanguage ? { color: '#b0021b'} : null}
+              key={lang}
+              onClick={this.updateLanguage.bind(null, lang)}
+            >
+              {lang}
+            </li>
+          )
+        })}
+      </ul>
     )
   }
 }
