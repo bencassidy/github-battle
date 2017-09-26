0736fb0826e688eeb8536cd7907c109d8215972a
bencassidy <ben.cassidy11@gmail.com>
2017-09-25 17:14:46 -0700

    added routes, skeleton for battle

--- app/components/App.js
+++ app/components/App.js
@@ -1,11 +1,15 @@
 const React = require('react');
-const Popular = require('./Popular');
 const ReactRouter = require('react-router-dom');
 
 const Router = ReactRouter.BrowserRouter;
 const Route = ReactRouter.Route;
+const Switch = ReactRouter.Switch;
 
 const Nav = require('./Nav');
+const Home = require('./Home');
+const Battle = require('./Battle');
+const Popular = require('./Popular');
+
 
 class App extends React.Component {
   render() {
@@ -13,7 +17,12 @@ class App extends React.Component {
       <Router>
         <div className='container'>
           <Nav/>
-          <Route path='/popular' component={Popular} />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/battle' component={Battle} />
+            <Route path='/popular' component={Popular} />
+            <Route render={() => <p>Not Found</p>}/>
+          </Switch>
         </div>
       </Router>
     )
